59a8c204d498e4930f74cff45577f9ef0969faf8
Lizzy2653 <147071082+Lizzy2653@users.noreply.github.com>
2023-10-26 13:31:37 -0400

    save changes

--- Main.java
+++ Main.java
@@ -7,30 +7,44 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
-        public record FinancialEntry(String date, String time, String description, String vendor, Double amount);
+    public static void main(String[] args) {
+        public record FinancialEntry(String date, String time, String description, String vendor, Double amount) ;
 
-        scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         char choice;
 
-        do {System.out.println("Home Screen");
-            System.out.println("D)") "Add Deposit");"
-            System.out.println("P);
+        do {
+            System.out.println("Home Screen");
+            System.out.println("D" + "Add Deposit");
+            System.out.println("P");
             System.out.println("L) Ledger");
             System.out.println("X) Exit");
             System.out.print("Enter your choice: ");
 
-            choice = scanner.next().charAt(0); scanner.nextLine();
+            choice = scanner.next().charAt(0);
+            scanner.nextLine();
 
-            switch (Character.toUpperCase(choice)) { case 'D': System.out.println("You chose to Add Deposit.");
+            switch (Character.toUpperCase(choice)) {
+                case 'D':
+                    System.out.println("You chose to Add Deposit.");
+                    break;
+                case 'P':
+                    System.out.println("You chose to Make Payment (Debit).");
+                    break;
+                case 'L':
+                    System.out.println("You chose to view the Ledger.");
+                    break;
+                case 'X':
+                    System.out.println("Exiting the program. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
 
-            break; case 'P': System.out.println("You chose to Make Payment (Debit).") break; case 'L': System.out.println("You chose to view the Ledger.");
-            break; case 'X': System.out.println("Exiting the program. Goodbye!");
-            break; default: System.out.println("Invalid choice. Please try again.");
-            break; }
-
-            } while (Character.toUpperCase(choice) != 'X'); scanner.close(); } }
+        } while (Character.toUpperCase(choice) != 'X'); scanner.close();
+    }
 
 }
 
-    }
+
